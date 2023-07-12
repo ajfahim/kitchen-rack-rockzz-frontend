1a24940a1702c24c4b659e76451f9b9385a9cbaf
ajfahim <ajfahim52@gmail.com>
2023-07-12 21:47:45 +0600

    node env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    backend_url: "http://localhost:5000/api",
-  },
+    reactStrictMode: true,
+    env: {
+        backend_url:
+            process.env.NODE_ENV === 'development'
+                ? 'http://localhost:5000/api'
+                : 'https://kitchen-rack-rockzz-backend-dev.vercel.app/api',
+    },
 };
 
 module.exports = nextConfig;
